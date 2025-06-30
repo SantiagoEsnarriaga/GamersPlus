fb593d0fd89ded2f047c1df9a37cd9056f2aebb7
SantiagoEsnarriaga <santiagooesnarriaga@gmail.com>
2025-06-30 00:17:34 -0300

    Delete src directory

--- src/database/ListaDeseadosDAO.java
+++
@@ -1,9 +0,0 @@
-package database;
-
-import java.util.List;
-
-public interface ListaDeseadosDAO {
-    List<Integer> getVideojuegosIds(int listaId);
-    boolean addVideojuegoToLista(int listaId, int videojuegoId);
-    boolean removeVideojuegoFromLista(int listaId, int videojuegoId);
-}
